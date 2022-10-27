65679e7b092085b687b054056f113e7ddca71d7c
costalucs <lucasdeslz@hotmail.com>
2022-10-27 00:17:03 -0300

    [feat]: match service

---
+++ app/backend/src/services/MatchServices.ts
@@ -0,0 +1,24 @@
+import Team from '../database/models/Team';
+import { IMatch } from '../interfaces/IMatch';
+import Match from '../database/models/Match';
+
+export default class MatchServices {
+  private model = Match;
+  getAll = async (): Promise<IMatch[] | null> => {
+    const matches = await this.model.findAll({
+      include: [
+        {
+          model: Team,
+          as: 'teamHome',
+          attributes: { exclude: ['id'] },
+        },
+        {
+          model: Team,
+          as: 'teamAway',
+          attributes: { exclude: ['id'] },
+        },
+      ],
+    });
+    return matches;
+  };
+}
