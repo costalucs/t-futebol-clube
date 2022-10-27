4beccf34d86e90b26a20f67625519a91597ff7d3
costalucs <lucasdeslz@hotmail.com>
2022-10-27 00:18:28 -0300

    index route import

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import loginRoute from './loginRoute';
+import matchRoutes from './matchRoute';
 import teamRoutes from './teamRoute';
 
 const routes = Router();
 
 routes.use(loginRoute);
 routes.use(teamRoutes);
+routes.use(matchRoutes);
 
 export default routes;
