20a3c191190898e4cdc83b7ca8cd60b1003a7519
costalucs <lucasdeslz@hotmail.com>
2022-10-28 15:06:31 -0300

    [feat]: matchcontroller > addmatch and finishmatch

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -20,4 +20,28 @@ export default class MatchController {
 
     return res.status(200).json(matches);
   };
+
+  addMatch = async (req: Request, res: Response) => {
+    const match = req.body;
+
+    if (match.homeTeam === match.awayTeam) {
+      return res
+        .status(422).json({ message: 'It is not possible to create a match with two equal teams' });
+    }
+    let infos;
+    try {
+      infos = await matchServices.createMatch(match);
+    } catch (err) {
+      return res.status(404).json({ message: 'There is no team with such id!' });
+    }
+    console.log(infos);
+
+    return res.status(201).json(infos);
+  };
+
+  finishMatch = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    await matchServices.finishMatch(id);
+    return res.status(200).json({ message: 'Finished' });
+  };
 }
