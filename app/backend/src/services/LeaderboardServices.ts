bf4148b156acbe8da92fe1facea80a8405f3dcba
costalucs <lucasdeslz@hotmail.com>
2022-10-31 14:06:40 -0300

    [feat]: leaderboard service get away table

--- app/backend/src/services/LeaderboardServices.ts
+++ app/backend/src/services/LeaderboardServices.ts
@@ -1,10 +1,16 @@
+import { ILeaderboard } from '../interfaces/ILeaderboard';
 import db from '../database/models';
-import getAllHomeTable from '../utils/querys';
+import { getAllAwayTable, getAllHomeTable } from '../utils/querys';
 
 export default class LeaderBoardServices {
-  getAllHome = async () => {
+  getAllHome = async (): Promise<ILeaderboard[] | null> => {
     const [table] = await db.query(getAllHomeTable);
 
-    return table;
+    return table as ILeaderboard[];
+  };
+
+  getAllAway = async (): Promise<ILeaderboard[] | null> => {
+    const [table] = await db.query(getAllAwayTable);
+    return table as ILeaderboard[];
   };
 }
