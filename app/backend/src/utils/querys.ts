d5f02669d82d789244e9e857b23e4afcf2c8f090
costalucs <lucasdeslz@hotmail.com>
2022-10-31 14:06:52 -0300

    query get away table

--- app/backend/src/utils/querys.ts
+++ app/backend/src/utils/querys.ts
@@ -1,4 +1,4 @@
-const getAllHomeTable = `
+export const getAllHomeTable = `
 SELECT 
     team.team_name name,
     SUM(matches.home_team_goals > matches.away_team_goals) * 3 
@@ -22,4 +22,26 @@ WHERE
 GROUP BY team.team_name
 ORDER BY totalPoints DESC , totalVictories DESC , goalsBalance DESC , goalsFavor DESC;`;
 
-export default getAllHomeTable;
+export const getAllAwayTable = `
+SELECT 
+    team.team_name name,
+    SUM(matches.away_team_goals > matches.home_team_goals) * 3 
+    + SUM(matches.home_team_goals = matches.away_team_goals) totalPoints,
+    COUNT(matches.away_team) totalGames,
+    SUM(matches.away_team_goals > matches.home_team_goals) 'totalVictories',
+    SUM(matches.away_team_goals = matches.home_team_goals) 'totalDraws',
+    SUM(matches.away_team_goals < matches.home_team_goals) 'totalLosses',
+    SUM(matches.away_team_goals) 'goalsFavor',
+    SUM(matches.home_team_goals) 'goalsOwn',
+    SUM(matches.away_team_goals - matches.home_team_goals) 'goalsBalance',
+    ((SUM(matches.away_team_goals > matches.home_team_goals) * 3 
+    + SUM(matches.home_team_goals = matches.away_team_goals)) / 
+    ((COUNT(matches.away_team)) * 3) * 100) efficiency
+FROM
+    teams team
+        INNER JOIN
+    matches matches ON team.id = matches.away_team
+WHERE
+    matches.in_progress = FALSE
+GROUP BY team.team_name
+ORDER BY totalPoints DESC , totalVictories DESC , goalsBalance DESC , goalsFavor DESC;`;
