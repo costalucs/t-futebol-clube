e25e60074e925f657964143c18cd535a6812d4e8
costalucs <lucasdeslz@gmail.com>
2022-11-09 10:19:18 -0300

    [feat]: swagger configs

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -3,6 +3,7 @@ import leaderboardRoute from './leaderboardRoutes';
 import loginRoute from './loginRoute';
 import matchRoutes from './matchRoute';
 import teamRoutes from './teamRoute';
+import swaggerRoute from './swaggerRoute';
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use(loginRoute);
 routes.use(teamRoutes);
 routes.use(matchRoutes);
 routes.use(leaderboardRoute);
+routes.use(swaggerRoute);
 
 export default routes;
