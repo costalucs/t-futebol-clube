1071d165ca43ec462d2d507d1ae53b009cc71586
costalucs <lucasdeslz@gmail.com>
2022-11-10 17:42:34 -0300

    [update]: change match post route

--- app/backend/src/routes/matchRoute.ts
+++ app/backend/src/routes/matchRoute.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import AuthToken from '../middlewares/AuthToken';
 import MatchController from '../controllers/MatchController';
+import AuthToken from '../middlewares/AuthToken';
 
 const matchRoutes = Router();
 const matchController = new MatchController();
@@ -9,6 +9,6 @@ const middleware = new AuthToken();
 matchRoutes.get('/matches', matchController.getAll);
 matchRoutes.patch('/matches/:id', matchController.updateMatch);
 matchRoutes.patch('/matches/:id/finish', matchController.finishMatch);
-matchRoutes.post('/matches', middleware.verifyToken, matchController.addMatch);
+matchRoutes.post('/match', middleware.verifyToken, matchController.addMatch);
 
 export default matchRoutes;
